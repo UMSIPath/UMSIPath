44b016c864f1d798d3a055b68b255858869dfa3a
Maura- <myoungman@pulitzercenter.org>
2013-03-18 16:50:10 -0300

    Update checklist.php

--- testCode/checklist.php
+++ testCode/checklist.php
@@ -42,10 +42,10 @@ echo breadcrumbs();
 <div>
   <h1>Which of these things do you have? <br>Select all that apply. </h1>  
   <form action="form_action.php">
-    <input id="bike" type="checkbox" name="vehicle" value="Bike"><label for="bike">I have a bike</label><br>
-    <input id="car" type="checkbox" name="vehicle" value="Car"><label for="car">I have a car</label><br>
-    <input id="rollerblades" type="checkbox" name="vehicle" value="Rollerblades"><label for="rollerblades">I have rollerblades</label><br>
-    <input id="donut" type="checkbox" name="food" value="Donut"><label for="donut">I have a donut</label><br>
+    <input id="bike" type="checkbox" name="vehicle[]" value="Bike"><label for="bike">I have a bike</label><br>
+    <input id="car" type="checkbox" name="vehicle[]" value="Car"><label for="car">I have a car</label><br>
+    <input id="rollerblades" type="checkbox" name="vehicle[]" value="Rollerblades"><label for="rollerblades">I have rollerblades</label><br>
+    <input id="donut" type="checkbox" name="food[]" value="Donut"><label for="donut">I have a donut</label><br>
     <input type="submit" class="button" value="Next &raquo;">
   </form>
   
