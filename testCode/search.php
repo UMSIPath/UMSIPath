049ca93c346f2a8efa91a8ef484d02d22023119c
Ying Ying Liu <yying6@gmail.com>
2013-03-25 15:12:48 -0400

    Modified search.php, added styles-01.css
    in order for the checkbox list to work, I had to change the <label for>
    from $name to $id -- I think everything still works...please double
    check

--- testCode/search.php
+++ testCode/search.php
@@ -1,16 +1,28 @@
+<!DOCTYPE html>
+<head>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+  <link href="styles-01.css" rel="stylesheet" type="text/css">
+</head>
+
 <?php
 require_once "db.php";
 session_start();
 
 $result = mysql_query("SELECT * FROM skills"); 
 
-echo '<form action="results.php" method="post">'; 
+echo '
+<div>
+<h1>Which skills are you interested in?<br>Select all that apply.</h1>
+<form action="results.php" method="post">'; 
+
 while($row = mysql_fetch_array($result)) 
   { 
   	$name = $row[skill_name];
   	$id = $row[skill_id];
-	echo '<input id="' . $id . '" type="checkbox" name="selectedskills[]" value="' . $name . '" ><label for="' . $name . '" >' . $name . '</label><br>';
+	echo '<input id="' . $id . '" type="checkbox" name="selectedskills[]" value="' . $name . '" ><label for="' . $id . '" >' . $name . '</label><br>';
 }
-echo '<input type="submit" class="button" value="Next"></form>';
+
+echo '<input type="submit" class="button" value="Next"></form>
+</div>';
   
 ?>
