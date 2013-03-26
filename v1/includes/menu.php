49ccba7eedb1f5a92fbc7a956e121cbed91280e2
Edgar N <edgarn@umich.edu>
2013-03-26 18:39:55 -0400

    updated header.php and menu.php
    new header and php have links working correctly

--- v1/includes/menu.php
+++ v1/includes/menu.php
@@ -1,50 +1,57 @@
 <div class="menublock">
 <ul class="menu">
 <p>top navigation</p>
-<li>   <a href="/v1/index.php"> index</a></li>
-<li>   <a href="/v1/login.php"> login</a></li>
-<li>   <a href="/v1/logout.php"> logout</a></li>
-<li>   <a href="/v1/about.php"> about</a></li>
-<li>   <a href="/v1/contact.php"> contact</a></li>
-<li>   <a href="/v1/help.php"> help</a></li>
+<li>   <a href="/umsipath/v1/index.php"> index</a></li>
+<li>   <a href="/umsipath/v1/login.php"> login</a></li>
+<li>   <a href="/umsipath/v1/logout.php"> logout</a></li>
+<li>   <a href="/umsipath/v1/about.php"> about</a></li>
+<li>   <a href="/umsipath/v1/contact.php"> contact</a></li>
+<li>   <a href="/umsipath/v1/help.php"> help</a></li>
 </ul>
 <br>
 <ul class="menu">
 <p>skills navigation</p>
+<<<<<<< HEAD
 <li>   <a href="../v1/skills/index.php"> index</a></li>
 <li>   <a href="../v1/skills/choose.php"> choose</a></li>
 <li>   <a href="../v1/skills/skills.php"> skills info</a></li>
 <li>   <a href="../v1/skills/recommendations.php"> recommendations</a></li>
+=======
+<li>   <a href="/umsipath/v1/skills/index.php"> index</a></li>
+<li>   <a href="/umsipath/v1/skills/choose.php"> choose</a></li>
+<li>   <a href="/umsipath/v1/skills/skills.php"> skills info</a></li>
+<li>   <a href="/umsipath/v1/skills/recommendations.php"> recommendations</a></li>
+>>>>>>> updated header.php and menu.php
 </ul>
 <br>
 <ul class="menu">
 <p>careers navigation</p>
-<li>   <a href="/v1/careers/index.php"> index</a></li>
-<li>   <a href="/v1/careers/choose.php"> choose</a></li>
-<li>   <a href="/v1/careers/careers.php"> careers info</a></li>
-<li>   <a href="/v1/careers/recommendations.php"> recommendations</a></li>
+<li>   <a href="/umsipath/v1/careers/index.php"> index</a></li>
+<li>   <a href="/umsipath/v1/careers/choose.php"> choose</a></li>
+<li>   <a href="/umsipath/v1/careers/careers.php"> careers info</a></li>
+<li>   <a href="/umsipath/v1/careers/recommendations.php"> recommendations</a></li>
 </ul>
 <br>
 <ul class="menu">
 <p>courses navigation</p>
-<li>   <a href="/v1/courses/index.php"> index</a></li>
-<li>   <a href="/v1/courses/choose.php"> choose</a></li>
-<li>   <a href="/v1/courses/courses.php"> courses info</a></li>
-<li>   <a href="/v1/courses/recommendations.php"> recommendations</a></li>
+<li>   <a href="/umsipath/v1/courses/index.php"> index</a></li>
+<li>   <a href="/umsipath/v1/courses/choose.php"> choose</a></li>
+<li>   <a href="/umsipath/v1/courses/courses.php"> courses info</a></li>
+<li>   <a href="/umsipath/v1/courses/recommendations.php"> recommendations</a></li>
 </ul>
 <br>
 <ul class="menu">
 <p>includes</p>
-<li>   <a href="/v1/includes/header.php"> header(no text)</a></li>
-<li>   <a href="/v1/includes/footer.php"> footer</a></li>
-<li>   <a href="/v1/includes/checklist.php"> checklist</a></li>
-<li>   <a href="/v1/includes/breadcrumbs.php"> breadcrumbs</a></li>
-<li>   <a href="/v1/includes/menu.php"> menu</a></li>
+<li>   <a href="/umsipath/v1/includes/header.php"> header(no text)</a></li>
+<li>   <a href="/umsipath/v1/includes/footer.php"> footer</a></li>
+<li>   <a href="/umsipath/v1/includes/checklist.php"> checklist</a></li>
+<li>   <a href="/umsipath/v1/includes/breadcrumbs.php"> breadcrumbs</a></li>
+<li>   <a href="/umsipath/v1/includes/menu.php"> menu</a></li>
 </ul>
 <br>
 <ul class="menu">
 <p>static: images and css </p>
-<li>   <a href="/v1/css/styles.css"> styles.css file</a></li>
+<li>   <a href="/umsipath/v1/css/styles.css"> styles.css file</a></li>
 </ul>
 <br>
 
