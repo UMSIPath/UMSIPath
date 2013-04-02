0d9d15805bc2a0400aa363c9186f3fbaf9c3caba
Emily Reynolds <ek.rnlds@gmail.com>
2013-04-02 17:45:52 -0400

    ying's changes

---
+++ v1/includes/navigation.php
@@ -0,0 +1,54 @@
+<header>
+    	<div id="header-wrap">
+        	<div id="logo">
+            	<a href="http://localhost:8888/umsipath/v1/index.php"><img src="http://localhost:8888/umsipath/v1/static/images/umsi-discover-logo.png" name="UMSI/Discover logo" alt="UMSI/Discover logo"/></a>
+            </div>
+        
+                        
+            <div id="metanav">
+            	<ul class="metanav">
+                	<li><a href="#">Howdy, Stranger</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </div>
+            
+            <div id="search-bar">
+            	<div id="search-box">
+             
+  					<form method="get" action="/search" id="search">
+  					<input name="q" type="text" size="40" />
+					</form>
+				</div>
+            </div>
+            
+            <nav>
+            	<ul>
+                	<li><a href="http://localhost:8888/umsipath/v1/index.php">Home</a></li>
+                    <li><a href="#">Discover</a>
+                    	<ul>
+                        	<li><a href="http://localhost:8888/umsipath/v1/skills/search.php"><h1>Find Courses by Skills</h1><p>You choose skills, we find you courses</p></a></li>
+							<li><a href="#"><h1>Find Courses by Careers</h1><p>You choose careers, we find you courses</p></a></li>
+						</ul>
+                    </li>
+                    <li><a href="#">Browse</a>
+                    	<ul>
+                        	<li><a href="#"><h1>All Courses</h1></a></li>
+                        	<li><a href="http://localhost:8888/umsipath/v1/skills/index.php"><h1>All Skills</h1></a></li>
+                        	<li><a href="#"><h1>All Careers</h1></a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Links</a>
+                    	<ul>
+                        	<li><a href="#"><h1>UMSI Homepage</h1></a></li>
+                            <li><a href="#"><h1>UMSI Career Development Office</h1></a></li>
+							<li><a href="#"><h1>TAPS</h1></a></li>
+                            <li><a href="#"><h1>Wolverine Access</h1></a></li>
+                    	</ul>
+                    </li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Help</a></li>
+                </ul>
+            </nav>
+   
+        </div>
+    </header>
