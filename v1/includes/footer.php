affd704a171383dedd35eeb3941108888ca242ec
Ying Ying Liu <yying6@gmail.com>
2013-04-02 17:24:23 -0400

    asdf

--- v1/includes/footer.php
+++ v1/includes/footer.php
@@ -1,4 +1,35 @@
-<?php
-echo 'this is the footer';
+<div id="footer-top-wrap">
+    	<footer id="top">
+        	
+            <div class="footer-widget">
+        	<h1 class="footer">A bit about us...</h1>
+            <p>UMSI/Discover is a joint project between the UMSI Path Group, UMSI Career Development Office, and a learning analytics fellowship from UM's Center for Research on Learning and Teaching (CLRT).</p>
+            </div>
+            
+            <div class="footer-widget">
+        	<h1 class="footer">Contact Us</h1>
+            <p>UMSI Path Group<br>
+Learning Analytics Fellows<br>
+Center for Research on Learning & Teaching<br>
+University of Michigan<br>
+Ann Arbor, MI 48109<br><br>
 
-?>
+E | umsi-path@googlegroups.com</p>
+            </div>
+            
+            <div id="footer-sm">
+            	<div id="sm-fb"><a href="#">Facebook</a></div>
+            	<div id="sm-twitter"><a href="#">Twitter</a></div>
+                <div id="sm-linkedin"><a href="#">LinkedIn</a></div>
+            	<p>Made in Michigan</p>
+            	<p>GO BLUE!</p>
+            </div>
+            
+        </footer>
+    </div>
+    
+    <div id="footer-bottom-wrap">
+        <footer id="bottom">
+        <p>&copy; 2013 UMSI Path</p>
+        </footer>
+    </div>
