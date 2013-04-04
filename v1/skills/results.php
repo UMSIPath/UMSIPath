5a9b6c606973209b3298666545fcde9b23431067
Ying Ying Liu <yying6@gmail.com>
2013-04-03 23:37:19 -0400

    change file names

---
+++ v1/skills/results.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<?php include '../includes/header.php'; ?>
+</head>
+
+<body>
+<?php include '../includes/menu.php'; ?>
+
+<div class="main">
+<h1>Careers Recommendations</h1>
+<p><?php include '../includes/breadcrumbs.php'; ?></p>
+<p> Lorem Ipsum stuff... </p>
+
+
+</div>
+<?php include '../includes/footer.php'; ?>
+</body>
+</html>
