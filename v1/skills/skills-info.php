85b434654a669893c5c3865910fb71cecc91fa46
Ying Ying Liu <yying6@gmail.com>
2013-04-08 15:35:32 -0400

    started working on individual skills page

--- v1/skills/skills-info.php
+++ v1/skills/skills-info.php
@@ -17,7 +17,40 @@
         
         <div class="breadcrumbs-wrap">
         	<div class="breadcrumbs">
-            	<h3 class="breadcrumbs"><a href="#">< &ensp;Back to Browse All Skills</a></h3>
-                <span class="blue"></span>
+            	<h3 class="breadcrumbs"><a href="index.php">< &ensp; Back to Browse All Skills</a></h3>
             </div>
         </div>
+        
+        <div id="content-wrap">
+        	<div id="content">
+            	<div class="section-info">
+                	<h3>Intro sentence goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi volutpat ornare augue, in vehicula est tempus eu. Sed lorem ante, mattis nec pretium dignissim, porta non mi. Pellentesque elementum turpis ut nunc mollis bibendum. Quisque convallis massa at felis blandit ornare. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sollicitudin lacus eu risus tempor varius.<br /><br />
+
+Quisque risus turpis, auctor nec vestibulum at, eleifend id sapien. Aliquam erat volutpat. Ut sollicitudin sollicitudin metus vitae suscipit. Nulla volutpat orci vitae augue mollis semper dapibus nunc imperdiet. Aliquam velit magna, porta non lobortis non, posuere sit amet nisl. Duis leo felis, tristique a cursus nec, vestibulum at lacus. Curabitur justo enim, tincidunt vitae venenatis nec, ornare et dolor. In hac habitasse platea dictumst.</p>
+            	</div>
+            	
+                <div class="section-rsidebar">
+                	<div class="related">
+                    	<h4>Related Courses</h4>
+                    	<p>sidebar text</p>
+                     </div>
+                    
+                    <div class="related">
+                    	<h4>Related Careers</h4>
+                    	<p>textttt </p>
+                    </div>
+                </div>
+			</div><!-- end content -->        
+        
+        
+        </div><!-- end content-wrap -->
+        
+    </div><!-- end main -->
+</div><!-- end container -->
+
+<?php include('../includes/footer.php'); ?>
+
+</div>
+</body>
+</html>
