cb515f43286cec915a077c47d75343b78e7a4119
Ying Ying Liu <yying6@gmail.com>
2013-04-10 17:11:14 -0400

    stuff

---
+++ v1/careers/results-copy.php
@@ -0,0 +1,44 @@
+<?php include('../includes/head.php'); ?>
+    <title>UMSI / Discover: Find Courses by Careers</title>
+    </head>
+
+
+<body>
+
+<div id="container">
+    <?php include('../includes/navigation.php'); ?>
+    <div id="main">
+    	<div id="careers-header-wrap">
+        	<div id="careers-header">
+            	<div id="section-icon"><img src="../static/images/section-icon-careers.png" /></div>
+            	<div id="section-header"><h1 class="section-header-double">Based on the skills you chose, we think these courses will interest you...</h1></div>
+                <div id="learnmore" class="careers"><p><a href="index.php">Want to learn more about these skills? Check them out here.</a></p></div>
+            </div>
+        </div>
+        
+        <div class="breadcrumbs-wrap">
+        	<div class="breadcrumbs">
+            	<h3 class="breadcrumbs"><a href="../index.php">Home</a> <span>&ensp;/&ensp;</span> <a href="search.php">Choose Careers</a> <span>&ensp;/&ensp;</span> Results</h3>
+            </div>
+        </div>
+        
+        <div id="content-wrap">
+        	<div id="content">
+           		
+				
+				
+	
+                
+            </div><!-- end content -->
+        </div><!-- end content wrap -->
+        
+        
+   	
+    </div> <!-- end main -->
+</div> <!-- end container -->
+    
+        
+<?php include('../includes/footer.php'); ?>
+
+</body>
+</html>
